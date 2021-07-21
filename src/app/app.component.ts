cfc09dde7e5f06f725b5e1d7e8fc413227758ea1
Sebastian <srankoin@gmail.com>
2021-07-20 22:58:13 -0500

    concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JokeService } from './Services/joke.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jokeapp';
+  joke = null
+  constructor(private jokeService : JokeService){}
+
+  grabJoke(){
+    this.jokeService.getJoke().subscribe(data => this.joke = data)
+  }
 }
