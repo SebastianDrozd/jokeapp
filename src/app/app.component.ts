fa7dfa11c85d6a82df82714c9c9f78077fe8675f
Sebastian <srankoin@gmail.com>
2021-07-21 17:28:43 -0500

    Better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,12 @@ export class AppComponent {
   title = 'jokeapp';
   joke = null
   mad = false;
+  intro = true;
   constructor(private jokeService : JokeService){}
 
   grabJoke(){
     this.jokeService.getJoke().subscribe(data => {this.joke = data;this.mad = false})
+    this.intro = false;
   }
   lamed(){
     this.mad = true;
